20659a36dc4a12f8bb6ba5ada1b1241b4d271cf7
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2025-12-25 11:08:54 +0600

    Grade Checker

---
+++ Conditions-DecisionMaking/grade-checker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+read -p "Enter marks: " mark
+
+if (( mark >= 80 && mark <= 100 )); 
+then
+    echo "Grade A+"
+elif (( mark >= 70 && mark < 80 )); 
+then
+    echo "Grade A"
+elif (( mark >= 60 && mark < 70 )); 
+then
+    echo "Grade A-"
+elif (( mark >= 50 && mark < 60 )); 
+then
+    echo "Grade B"
+elif (( mark >= 40 && mark < 50 )); 
+then
+    echo "Grade C"
+elif (( mark >= 33 && mark < 40 )); 
+then
+    echo "Grade D"
+else
+    echo "Fail"
+fi
