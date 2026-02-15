c3eac41eb2bc1c787ce955ca55fdf851e0da32ad
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2026-02-15 15:12:25 +0600

    Add Right-Angled Number Triangle with Increment bash script

---
+++ Pattern-Printing/rightangled-increment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Enter number of rows: " rows
+num=1
+
+for (( i=1; i<=rows; i++ ))
+do
+    for (( j=1; j<=i; j++ ))
+    do
+        echo -n "$num "
+        ((num++))
+    done
+    echo
+done
